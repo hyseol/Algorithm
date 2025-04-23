9b3f4e196f9bcb44b96efe1284ac1cbfad4d406f
SEOL <hyseol99@gmail.com>
2025-04-23 21:35:58 +0900

    Update 같은 숫자는 싫어.py

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,9 +1,15 @@
-from collections import deque
-
 def solution(arr):
     answer = []
     for i in range(len(arr)):
         if i == 0 or arr[i] != arr[i-1]:
             answer.append(arr[i])
-            
     return answer
+
+
+# stack
+def solution(arr):
+    stack = []
+    for num in arr:
+        if not stack or stack[-1] != num:
+            stack.append(num)
+    return stack
