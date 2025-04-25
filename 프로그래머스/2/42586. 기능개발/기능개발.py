2f0f1ed4a98fbcd9edf099a1c621bf3752129b54
SEOL <hyseol99@gmail.com>
2025-04-25 20:39:12 +0900

    [level 2] Title: 기능개발, Time: 0.48 ms, Memory: 9.26 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -0,0 +1,20 @@
+def solution(progresses, speeds):
+    from collections import deque
+    queue = deque(progresses)
+    answer = []
+    day = 0
+
+    while queue:
+        for i in range(len(queue)):
+            queue[i] += speeds[i]
+
+        release = 0
+        while queue and queue[0] >= 100:
+            queue.popleft()
+            speeds.pop(0)
+            release += 1
+
+        if release > 0:
+            answer.append(release)
+    
+    return answer
