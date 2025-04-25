4dccae09b560a29a33028524f4a61f48c81edb43
SEOL <hyseol99@gmail.com>
2025-04-25 21:52:37 +0900

    Update 기능개발.py

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -3,17 +3,43 @@ def solution(progresses, speeds):
     queue = deque(progresses)
     answer = []
     
-    while queue:
+    while queue: # 하루 지날때마다 작업 진도 업데이트
         for i in range(len(queue)):
             queue[i] += speeds[i]
 
         release = 0
-        while queue and queue[0] >= 100:
+        while queue and queue[0] >= 100: 
             queue.popleft()
             speeds.pop(0)
-            release += 1
+            release += 1 # 100 이상일 경우 배포 +1
 
         if release > 0:
-            answer.append(release)
+            answer.append(release) # answer에 release 값 저장
     
     return answer
+
+
+
+# zip 활용 / 필요한 작업 일수로 계산 버전
+
+def solution(progresses, speeds):
+    from collections import deque
+    import math
+
+    days = deque()
+    answer = [] 
+    for p, s in zip(progresses, speeds): # 필요한 작업 일수 계산
+        days.append(math.ceil((100-p)/s)) 
+
+    while days:
+        current = days.popleft() # 첫번째 작업 배포
+        release = 1
+
+        while days and days[0] <= current: # 첫번째 작업보다 작업일수가 작거나 같으면 배포
+            days.popleft()
+            release += 1
+        
+        answer.append(release)
+    
+    return answer
+
