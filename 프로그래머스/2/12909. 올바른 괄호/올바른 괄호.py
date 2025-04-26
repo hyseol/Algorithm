6c956b178bc609c600debd7d4478a01a9e11347e
SEOL <hyseol99@gmail.com>
2025-04-26 14:06:19 +0900

    Update 올바른 괄호.py

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,5 +1,18 @@
 def solution(s):
     stack = []
+    for i in s:
+        if i == "(":
+            stack.append(i)
+        elif i == ")": 
+            if not stack or stack[-1] != "(":
+                return False
+            stack.pop()
+    return not stack
+
+
+# pairs 활용 버전
+def solution(s):
+    stack = []
     pair = { "(" : ")"}
 
     for i in s:
@@ -8,3 +21,6 @@ def solution(s):
         elif len(stack) == 0 or i != pair[stack.pop()]:
             return False
     return len(stack) == 0
+
+
+
