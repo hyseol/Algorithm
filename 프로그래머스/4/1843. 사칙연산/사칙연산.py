c4246ecc5350305534fa8ee5494d21516b86cfe6
SEOL <hyseol99@gmail.com>
2025-05-10 14:01:36 +0900

    [level 4] Title: 사칙연산, Time: 2.39 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/사칙연산.py
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.py
@@ -1,31 +1,15 @@
 def solution(arr):
-    n = (len(arr) // 2) + 1  # 숫자의 개수
+    arrs = ''.join(arr).split('-')  # -를 기준으로 나눔
+    val0 = sum(list(map(int, arrs[0].split('+'))))  # 맨 앞의 값 = 무조건 포함
+    if len(arrs) == 1:  # -가 없는 경우
+        return val0
+    
+    min_val = 0
+    max_val = 0
+    for arr in arrs[:0:-1]:  # 뒤에서부터 앞으로 순회
+        x = list(map(int, arr.split('+')))
+        x_min = -(sum(x))  # -로 묶어서 최소값 계산
+        x_max = sum(x[1:]) - x[0]  # x[0]만 빼고 나머지는 다 더함 = 최댓값
+        min_val, max_val = min(x_min + min_val, x_min - max_val), max(x_max + max_val, x_min - min_val)
 
-    # max, min 함수 사용을 위해 inf 값을 초기값으로 사용
-    dp_max = [[float('-inf')] * n for _ in range(n)]
-    dp_min = [[float('inf')] * n for _ in range(n)]
-
-    for i in range(n):
-        dp_max[i][i] = dp_min[i][i] = int(arr[2*i])  # 대각선 위치 = 길이 1 구간
-
-    for length in range(2, n+1):  # 구간 길이
-        for i in range(n - length + 1):  # i = 시작 인덱스 / n - length + 1 = 시작 인덱스가 될 수 있는 마지막 인덱스
-            j = i + length -1  # j = 끝 인덱스
-            for k in range(i, j):  # k를 기준으로 왼쪽, 오른쪽 나눔
-                # print(i,j)
-                op = arr[2 * k + 1]  # 연산자 
-                for a in (dp_min[i][k], dp_max[i][k]):
-                    for b in (dp_min[k+1][j], dp_max[k+1][j]):
-                        # print(a, b, k)
-                        if op == '+':
-                            val = a + b
-                        elif op == '-':
-                            val = a - b
-                        elif op == '*':
-                            val = a * b 
-                        dp_max[i][j] = max(dp_max[i][j], val)
-                        dp_min[i][j] = min(dp_min[i][j], val)
-                        # print(dp_max)
-                        # print(dp_min)
-
-    return dp_max[0][n-1]
+    return val0 + max_val
